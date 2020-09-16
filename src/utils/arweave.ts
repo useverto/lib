63d4a1de1eb662186daa7c836cd28b9d846940c9
divy-work <dj.srivastava23@gmail.com>
2020-09-16 23:08:53 +0530

    fix: change logger methods

--- src/utils/arweave.ts
+++ src/utils/arweave.ts
@@ -1,6 +1,5 @@
 import Arweave from "arweave";
 import { JWKPublicInterface } from "arweave/node/lib/wallet";
-import Logger from "@utils/logger";
 import { relative } from "path";
 import { readFile } from "fs/promises";
 import Community from "community-js";
@@ -10,10 +9,6 @@ const relativeKeyPath = process.env.KEY_PATH
   ? relative(__dirname, process.env.KEY_PATH)
   : "./arweave.json";
 
-const log = new Logger({
-  level: Logger.Levels.debug,
-  name: "arweave",
-});
 
 export async function init(keyfile?: string) {
   const client = new Arweave({
@@ -22,12 +17,12 @@ export async function init(keyfile?: string) {
     protocol: "https",
     timeout: 20000,
     logging: false,
-    logger: (msg: any) => log.debug(msg),
+    logger: console.log,
   });
   const jwk = await getJwk(keyfile);
   const walletAddr = await client.wallets.jwkToAddress(jwk!);
   const info = await client.network.getInfo();
-  log.info(
+  console.log(
     "Created Arweave instance:\n\t\t" +
       `wallet_address=${walletAddr}\n\t\t` +
       `block_height=${info.height}\n\t\t` +
@@ -35,9 +30,9 @@ export async function init(keyfile?: string) {
       `node_state_latency=${info.node_state_latency}`
   );
 
-  log.info("Configuring community.xyz");
+  console.log("Configuring community.xyz");
   const community = new Community(client, jwk);
-  log.debug(
+  console.log(
     `Setting community tx. community_tx=${CONSTANTS.exchangeContractSrc}`
   );
   await community.setCommunityTx(CONSTANTS.exchangeContractSrc);
@@ -48,7 +43,7 @@ export async function init(keyfile?: string) {
 let cachedJwk: JWKPublicInterface | undefined;
 export async function getJwk(keyfile?: string) {
   if (!cachedJwk) {
-    log.info(`Loading keyfile from: ${keyfile || relativeKeyPath}`);
+    console.log(`Loading keyfile from: ${keyfile || relativeKeyPath}`);
     const potentialJwk = JSON.parse(
       await readFile(keyfile || relativeKeyPath, { encoding: "utf8" })
     );
