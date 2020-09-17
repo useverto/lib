bc6b933377957dbcb4d704bdebf4d59498ee5d9c
divy-work <dj.srivastava23@gmail.com>
2020-09-17 19:19:10 +0530

    feat: add basic class contructor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-// TODO
+import { getTokens } from "./get_tokens";
+import { getTradingPosts } from "./get_trading_posts";
+import Arweave from "arweave";
+
+export default class Verto {
+    public arweave!: Arweave;
+    
+    constructor(arweave: Arweave) {
+        this.arweave = arweave;
+    }
+}
