cd4a642dbb1bb59651c61cc9c6fbc7a6a6288cb2
John Letey <johnletey@gmail.com>
2020-09-17 17:25:41 +0100

    feat: parse markdown files directly

---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import getFuncs from "../lib/get-funcs";
+
+const Home = ({ funcs }) => {
+  console.log(funcs);
+
+  return (
+    <div>
+      <h1>Coming soon ...</h1>
+      {funcs.map((func) => (
+        <a href={`/func/${func.slug}`}>{func.title}</a>
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps = () => {
+  const funcs = getFuncs();
+
+  return {
+    props: {
+      funcs,
+    },
+  };
+};
+
+export default Home;
