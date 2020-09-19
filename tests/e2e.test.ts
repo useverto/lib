963df0477ae2acd7d48c5276aaf24f474c1252a8
divy-work <dj.srivastava23@gmail.com>
2020-09-19 20:49:23 +0530

    feat: complete tests

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,6 +1,9 @@
 import Verto from "../src";
 import { assert } from "chai";
 
+// Assign a token; Just for testing
+const TOKEN = "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A";
+
 let vertoInstance: Verto;
 
 describe("E2E Tests", () => {
@@ -9,5 +12,40 @@ describe("E2E Tests", () => {
     assert(vertoInstance);
     done();
   });
-  it("")
+  it("Test getTokens(<TOKEN>)", (done) => {
+    vertoInstance
+      .getTokens(TOKEN)
+      .then((res) => {
+        assert(res);
+        done();
+      })
+      .catch(done);
+  });
+  it("Test getTradingPosts()", (done) => {
+    vertoInstance
+      .getTradingPosts()
+      .then((res) => {
+        assert(res);
+        done();
+      })
+      .catch(done);
+  });
+  it("Test price(<TOKEN>)", (done) => {
+    vertoInstance
+      .price(TOKEN)
+      .then((res) => {
+        assert(res);
+        done();
+      })
+      .catch(done);
+  });
+  it("Test volume(<TOKEN>)", (done) => {
+    vertoInstance
+      .volume(TOKEN)
+      .then((res) => {
+        assert(res);
+        done();
+      })
+      .catch(done);
+  });
 });
