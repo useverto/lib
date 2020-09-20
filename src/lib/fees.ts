1c2fdd72f620fa7b3b1aea1d07d441e3523916c4
John Letey <johnletey@gmail.com>
2020-09-20 14:57:10 +0100

    feat: add remainder of fee functions

--- src/lib/fees.ts
+++ src/lib/fees.ts
@@ -1,9 +1,111 @@
 import Arweave from "arweave";
+import { JWKInterface } from "arweave/node/lib/wallet";
+import Transaction from "arweave/node/lib/transaction";
+import {
+  exchangeFee,
+  exchangeWallet,
+  exchangeContractSrc,
+} from "@utils/constants";
+import Community from "community-js";
 import { query } from "@utils/gql";
 import { EdgeQueryResponse } from "types";
 import genesisQuery from "../queries/genesis.gql";
-import { exchangeWallet } from "@utils/constants";
-import Transaction from "arweave/node/lib/transaction";
+
+const createExchangeFeeTx = async (
+  client: Arweave,
+  keyfile: JWKInterface,
+  amnt: number
+): Promise<Transaction> => {
+  const tags = {
+    Exchange: "Verto",
+    Type: "Fee-Exchange",
+  };
+  const fee = amnt * exchangeFee;
+
+  const tx = await client.createTransaction(
+    {
+      target: exchangeWallet,
+      quantity: client.ar.arToWinston(fee.toString()),
+    },
+    keyfile
+  );
+
+  for (const [key, value] of Object.entries(tags)) {
+    tx.addTag(key, value);
+  }
+
+  return tx;
+};
+
+const createTradingPostFeeTx = async (
+  client: Arweave,
+  keyfile: JWKInterface,
+  amnt: number,
+  pst: string,
+  post: string
+): Promise<Transaction> => {
+  const tradingPostFee = Math.ceil(
+    Math.ceil(amnt) * (await getTradingPostFee(client, post))
+  );
+
+  const tags = {
+    Exchange: "Verto",
+    Type: "Fee-Trading-Post",
+    "App-Name": "SmartWeaveAction",
+    "App-Version": "0.3.0",
+    Contract: pst,
+    Input: `{"function":"transfer","target":"${post}","qty":${tradingPostFee}}`,
+  };
+  const tx = await client.createTransaction(
+    {
+      target: post,
+      data: Math.random().toString().slice(-4),
+    },
+    keyfile
+  );
+
+  for (const [key, value] of Object.entries(tags)) {
+    tx.addTag(key, value);
+  }
+
+  return tx;
+};
+
+const createVRTHolderFeeTx = async (
+  client: Arweave,
+  keyfile: JWKInterface,
+  amnt: number,
+  pst: string
+): Promise<Transaction> => {
+  const community = new Community(client);
+  await community.setCommunityTx(exchangeContractSrc);
+  const tipReceiver = await community.selectWeightedHolder();
+
+  const fee = Math.ceil(Math.ceil(amnt) * exchangeFee);
+
+  const tags = {
+    Exchange: "Verto",
+    Type: "Fee-VRT-Holder",
+    "App-Name": "SmartWeaveAction",
+    "App-Version": "0.3.0",
+    Contract: pst,
+    Input: `{"function":"transfer","target":"${tipReceiver}","qty":${fee}}`,
+  };
+
+  const tx = await client.createTransaction(
+    {
+      target: tipReceiver,
+      data: Math.random().toString().slice(-4),
+    },
+    keyfile
+  );
+
+  for (const [key, value] of Object.entries(tags)) {
+    tx.addTag(key, value);
+  }
+
+  return tx;
+};
 
 const getTradingPostFee = async (
   client: Arweave,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,7 @@ export const exchangeContractSrc =
   "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A";
 export const exchangeWallet = "aLemOhg9OGovn-0o4cOCbueiHT9VgdYnpJpq7NgMA1A";
 
+export const exchangeFee = 0.005;
+
 // integers on the server are 32 bit so we can't user Number.MAX_SAFE_INTEGER
 export const maxInt = 2147483647;
