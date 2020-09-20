ceb1f057d214efd879ac9d2b9ff9e1f250b7e7f7
John Letey <johnletey@gmail.com>
2020-09-20 12:23:01 +0100

    feat: work on `createTrade` function

--- src/lib/trade.ts
+++ src/lib/trade.ts
@@ -1,8 +1,70 @@
 import Arweave from "arweave";
+import { JWKInterface } from "arweave/node/lib/wallet";
 import Transaction from "arweave/node/lib/transaction";
 
-export const createTrade = async () => {
-  // TODO
+export const createTrade = async (
+  client: Arweave,
+  keyfile: JWKInterface,
+  type: string,
+  amnt: number,
+  pst: string,
+  post: string,
+  rate?: number
+): Promise<Transaction | undefined> => {
+  // TODO(@johnletey): Add fee txs!!!
+
+  if (type.toLowerCase() === "buy") {
+    const tags = {
+      Exchange: "Verto",
+      Type: "Buy",
+      Token: pst,
+    };
+
+    const tx = await client.createTransaction(
+      {
+        target: post,
+        quantity: client.ar.arToWinston(amnt.toString()),
+      },
+      keyfile
+    );
+
+    for (const [key, value] of Object.entries(tags)) {
+      tx.addTag(key, value);
+    }
+
+    return tx;
+  }
+
+  if (type.toLowerCase() === "sell" && rate) {
+    const tags = {
+      Exchange: "Verto",
+      Type: "Sell",
+      "App-Name": "SmartWeaveAction",
+      "App-Version": "0.3.0",
+      Contract: pst,
+      Rate: 1 / rate,
+      Input: `{"function":"transfer","target":"${post}","qty":${Math.ceil(
+        amnt
+      )}}`,
+    };
+
+    const tx = await client.createTransaction(
+      {
+        target: post,
+        data: Math.random().toString().slice(-4),
+      },
+      keyfile
+    );
+
+    for (const [key, value] of Object.entries(tags)) {
+      // @ts-ignore
+      tx.addTag(key, value);
+    }
+
+    return tx;
+  }
+
+  return;
 };
 
 export const sendTrade = async () => {
