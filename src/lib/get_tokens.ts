a98ce1f8127914cc277ae61a3d795a880b3f5f1e
divy-work <dj.srivastava23@gmail.com>
2020-10-24 23:11:21 +0530

    fix: tests work

--- src/lib/get_tokens.ts
+++ src/lib/get_tokens.ts
@@ -27,11 +27,10 @@ export const getTokens = async (
   const IDs: { type: string; id: string }[] = [];
   tokenTxs.map((tx) => IDs.push({ type: "tx", id: tx.node.id }));
 
-  // TODO(@johnletey): Use `localPorridge` to grab custom tokens.
   // @ts-ignore
-    const storage = typeof storage == "undefined" ?  new localPorridge("./.cache.json") : localStorage;
-    const cache = JSON.parse(storage.getItem("customTokens") || "[]");
-    cache.map((entry: string) => IDs.push({ type: "contract", id: entry }));
+  const storage = typeof localStorage == "undefined" ?  new localPorridge("./.cache.json") : localStorage;
+  const cache = JSON.parse(storage.getItem("customTokens") || "[]");
+  cache.map((entry: string) => IDs.push({ type: "contract", id: entry }));
   const tokens: VertoToken[] = [];
   for (const entry of IDs) {
     const contractID =
